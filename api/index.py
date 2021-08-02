2317f788df947d48b6aedb6abf1191c1ba9e0286
leo-guinan <leo@guinantech.com>
2021-08-02 10:02:35 -0400

    capitalization problem?

--- api/index.py
+++ api/index.py
@@ -23,9 +23,8 @@ class handler(BaseHTTPRequestHandler):
 		self.end_headers()
 
 		if "user_id" in dic:
-			# cursor.execute("SELECT * FROM user WHERE id = %(user_id)s",{'user_id': dic["user_id"]})
-			cursor.execute("SELECT table_name, column_name, data_type FROM information_schema.columns;")
-			values_array = list(cursor.fetchall())
+			cursor.execute("SELECT * FROM User WHERE id = %(user_id)s",{'user_id': dic["user_id"]})
+			values_array = list(cursor.fetchone())
 			col_names = [desc[0] for desc in cursor.description]
 			message	= dict(zip(col_names, values_array))
 		else:
